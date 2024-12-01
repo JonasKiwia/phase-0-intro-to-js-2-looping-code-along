240010d7011892292097f64a3081bb970800411b
Jonas Kiwia <jonas.kiwia@student.moringaschool.com>
2024-12-01 13:27:33 +0300

    looping code in index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    // Empty array to store the messages
+    let thankYouMessages = [];
+  
+    // Loop through the array of names
+    for (let i = 0; i < names.length; i++) {
+      // thank-you message for each name
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  
+      // Adds message to array
+      thankYouMessages.push(message);
+    }
+  
+    // Returns the array of message
+    return thankYouMessages;
+  }
+  
+
+  const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
+
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrement the number 
+    }
+  }
+  
+
+  countDown(10);
+  
+  
